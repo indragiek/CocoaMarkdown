3ec394a85db11f3ade7f361ddbf7de68cb5b99b6
Indragie Karunaratne <i@indragie.com>
2015-01-14 22:40:57 -0700

    API for CMAttributedStringRenderer

---
+++ CocoaMarkdown/CMAttributedStringRenderer.m
@@ -0,0 +1,13 @@
+//
+//  CMAttributedStringRenderer.m
+//  CocoaMarkdown
+//
+//  Created by Indragie on 1/14/15.
+//  Copyright (c) 2015 Indragie Karunaratne. All rights reserved.
+//
+
+#import "CMAttributedStringRenderer.h"
+
+@implementation CMAttributedStringRenderer
+
+@end
