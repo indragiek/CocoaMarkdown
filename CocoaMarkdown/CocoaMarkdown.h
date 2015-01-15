8750dca2338307310cd564e41cc16ffd825244cd
Indragie Karunaratne <i@indragie.com>
2015-01-15 14:28:16 -0700

    Add Mac framework target

--- CocoaMarkdown/CocoaMarkdown.h
+++ CocoaMarkdown/CocoaMarkdown.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Indragie Karunaratne. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for CocoaMarkdown.
 FOUNDATION_EXPORT double CocoaMarkdownVersionNumber;
