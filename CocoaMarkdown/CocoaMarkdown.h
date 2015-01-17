e7f8a8cd94fd6198fe9a56abbe869cf92401d80d
Indragie Karunaratne <i@indragie.com>
2015-01-16 22:48:21 -0700

    Add Ono to public headers

--- CocoaMarkdown/CocoaMarkdown.h
+++ CocoaMarkdown/CocoaMarkdown.h
@@ -22,3 +22,5 @@ FOUNDATION_EXPORT const unsigned char CocoaMarkdownVersionString[];
 #import <CocoaMarkdown/CMNode.h>
 #import <CocoaMarkdown/CMParser.h>
 #import <CocoaMarkdown/CMTextAttributes.h>
+
+#import <CocoaMarkdown/Ono.h>
